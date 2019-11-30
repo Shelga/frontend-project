a8e7efa66276f7a5b149b0f5dd44bf50fc910ddc
Olga Melnikova <shelga.melnikova@gmail.com>
2019-11-30 18:04:55 +0000

    step seven

--- Makefile
+++ Makefile
@@ -10,3 +10,5 @@ ask:
 	npx babel-node src/bin/brain-even.js
 calc:
 	npx babel-node src/bin/brain-calc.js
+gcd:
+	npx babel-node src/bin/brain-gcd.js
--- src/logic.js
+++ src/logic.js
@@ -1,27 +1,30 @@
+import readlineSync from 'readline-sync';
 
-// const welcome = (task) => {
-//     console.log('Welcome to the Brain Games');
-//     console.log(task)
-//     const userHello = readlineSync.question('May I have your name? ');
-//     console.log(`Hello, ${userHello}!\n`);
-// };
+const playGame = (nameOfAPlayer, generator) => {
+  const maxTries = 3;
+  let rightTries = 0;
 
-// const maxTries = 3;
-// let rightAnswers = 0;
+  while (rightTries < maxTries) {
+    const obj = generator();
+    console.log(`Question: ${obj.task}`);
+    const userAnswer = readlineSync.question('Your answer: ');
 
-// const playGame () => { 
-//   while (rightAnswers < maxTries) {
-    
-//     console.log(`Question: ${number}`);
+    if (userAnswer === `${obj.rightAnswer}`) {
+      console.log('Correct!');
+      rightTries += 1;
+    } else {
+      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${obj.rightAnswer}'.\nLet's try again, ${nameOfAPlayer}!`);
+      rightTries = 0;
+    }
+  }
+  console.log(`Congratulations, ${nameOfAPlayer}!`);
+};
 
-//     const userAnswer = readlineSync.question('Your answer: ');
-//     if (userAnswer === ${tryAnswer}) {
-//       console.log('Correct!');
-//       rightAnswers += 1;
-//     } else {
-//       console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${tryAnswer}'.\nLet's try again, ${userHello}!`);
-//       rightAnswers = 0;
-//     }
-//   };
-//   console.log(`Congratulations, ${userHello}!`);
-// };
+export const startGame = (gameDescription, generator) => {
+  console.log('Welcome to the Brain Games');
+  console.log(gameDescription);
+  const nameOfPlayer = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${nameOfPlayer}!\n`);
+
+  playGame(nameOfPlayer, generator);
+};
